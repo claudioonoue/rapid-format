ba44135b4a5fd0a33b86c6ec6f416d19d87603fc
Claudio Onoue <co_2000@live.com>
2020-09-25 11:28:43 -0300

    Removed gitkraken

--- 2.programs.sh
+++ 2.programs.sh
@@ -15,7 +15,6 @@ sudo wget -O ./${DEB_FOLDER}/discord.deb "https://discord.com/api/download?platf
 sudo wget -O ./${DEB_FOLDER}/skype.deb "https://go.skype.com/skypeforlinux-64.deb"
 
 sudo wget -O ./${DEB_FOLDER}/mysql-workbench.deb "https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community_8.0.21-1ubuntu18.04_amd64.deb"
-sudo wget -O ./${DEB_FOLDER}/gitkraken.deb "https://release.gitkraken.com/linux/gitkraken-amd64.deb"
 
 sudo apt install ./${DEB_FOLDER}/*.deb -y
 
